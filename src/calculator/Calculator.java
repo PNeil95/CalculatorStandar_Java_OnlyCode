bd44e19d8e8cd887f00ee4fe308afc2744ad4118
PNeil95 <pvbn9518@gmail.com>
2021-12-06 21:46:14 -0500

    second part - design finish

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -26,7 +26,7 @@ public class Calculator extends JFrame implements ActionListener{
 	JButton btnEight = new JButton("8");
 	JButton btnNine = new JButton("9");
 	JButton btnRest = new JButton("-");
-	JButton btnMultiple = new JButton("x");
+	JButton btnMultiple = new JButton("*");
 	JButton btnCero = new JButton("0");
 	JButton btnDiv = new JButton("/");
 	JButton btnPunto = new JButton(".");
@@ -43,6 +43,8 @@ public class Calculator extends JFrame implements ActionListener{
 		add(pantalla); //para que aparezca en la vista
 		
 		//6.Propiedades de los Botones
+		
+		//First row
 		btnOne.setBounds(22, 83, 60, 60);
 		btnOne.setFont(new Font ("tahoma",Font.ITALIC,20));
 		add(btnOne);
@@ -58,7 +60,57 @@ public class Calculator extends JFrame implements ActionListener{
 		btnClear.setBounds(217, 83, 96, 60);
 		btnClear.setFont(new Font ("tahoma",Font.ITALIC,20));
 		add(btnClear);
+		
+		//Second Row
+		btnFour.setBounds(22, 153, 60, 60);
+		btnFour.setFont(new Font ("tahoma",Font.ITALIC,20));
+		add(btnFour);
+		
+		btnFive.setBounds(87, 153, 60, 60);
+		btnFive.setFont(new Font("tahoma",Font.ITALIC,20));
+		add(btnFive);
+		
+		btnSix.setBounds(152, 153, 60, 60);
+		btnSix.setFont(new Font ("tahoma",Font.ITALIC,20));
+		add(btnSix);
 
+		btnSuma.setBounds(217, 153, 96, 60);
+		btnSuma.setFont(new Font ("tahoma",Font.ITALIC,20));
+		add(btnSuma);
+		
+		//Third row
+		btnSeven.setBounds(22, 223, 60, 60);
+		btnSeven.setFont(new Font ("tahoma",Font.ITALIC,20));
+		add(btnSeven);
+		
+		btnEight.setBounds(87, 223, 60, 60);
+		btnEight.setFont(new Font("tahoma",Font.ITALIC,20));
+		add(btnEight);
+		
+		btnNine.setBounds(152, 223, 60, 60);
+		btnNine.setFont(new Font ("tahoma",Font.ITALIC,20));
+		add(btnNine);
+
+		btnRest.setBounds(217, 223, 96, 60);
+		btnRest.setFont(new Font ("tahoma",Font.ITALIC,20));
+		add(btnRest);
+		
+		//Four row
+		btnMultiple.setBounds(22, 293, 60, 60);
+		btnMultiple.setFont(new Font ("tahoma",Font.ITALIC,20));
+		add(btnMultiple);
+		
+		btnCero.setBounds(87, 293, 60, 60);
+		btnCero.setFont(new Font("tahoma",Font.ITALIC,20));
+		add(btnCero);
+		
+		btnDiv.setBounds(152, 293, 60, 60);
+		btnDiv.setFont(new Font ("tahoma",Font.ITALIC,20));
+		add(btnDiv);
+
+		btnPunto.setBounds(217, 293, 96, 60);
+		btnPunto.setFont(new Font ("tahoma",Font.ITALIC,20));
+		add(btnPunto);
 	}
 
 	public static void main(String[] args) {
